750f8090901ccc8b25c84d081251f81976aa8ef6
Vladimir S. Lavrik <vlavrik@Vladimirs-MacBook-Air.local>
2020-12-31 14:06:40 +0100

    publishing on test server is performed

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import setup
+import setuptools
 
-setup(name='flespi-gateway',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='flespi-gateway',
       version='0.0.1',
       description='The python wrapper of a gateway rest API of a flespi platform.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/vlavrik/flespi-gateway',
       author='Dr. Vladimir Lavrik',
       author_email='lavrikvladimir@gmail.com',
       license='BSD 3-Clause "New" or "Revised" License',
-      packages=['flespi_gateway'],
-      zip_safe=False)
+      packages=setuptools.find_packages(),
+      zip_safe=False,
+      python_requires='>=3.8')
