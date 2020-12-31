451c22b498f24c081349d9a77f00285067879762
Vladimir S. Lavrik <vlavrik@Vladimirs-MacBook-Air.local>
2020-12-31 13:32:19 +0100

    renaming

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flespi-gateway',
+      version='0.0.1',
+      description='The python wrapper of a gateway rest API of a flespi platform.',
+      url='https://github.com/vlavrik/flespi-gateway',
+      author='Dr. Vladimir Lavrik',
+      author_email='lavrikvladimir@gmail.com',
+      license='BSD 3-Clause "New" or "Revised" License',
+      packages=['flespi_gateway'],
+      zip_safe=False)
