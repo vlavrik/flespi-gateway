846a9d9c488f76ee1b1f847fa20934410cf830a9
Vladimir S. Lavrik <vlavrik@Vladimirs-MacBook-Air.local>
2021-01-02 09:56:44 +0100

    readme changed

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='flespi-gateway',
-      version='0.0.3',
+      version='0.0.4',
       description='The python wrapper of a gateway rest API of a flespi platform.',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='https://github.com/vlavrik/flespi-gateway',
-      author='Dr. Vladimir Lavrik',
+      author='Vladimir Lavrik',
       author_email='lavrikvladimir@gmail.com',
       license='BSD 3-Clause "New" or "Revised" License',
       packages=setuptools.find_packages(),
