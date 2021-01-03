5cb7a572272215e892db77158bbe36a36acc96fe
Vladimir S. Lavrik <vlavrik@Vladimirs-Air.fritz.box>
2021-01-03 21:10:37 +0100

    all get methods for device are implemented

--- flespi_gateway/gateway.py
+++ flespi_gateway/gateway.py
@@ -1,10 +1,10 @@
 """The Gateway API (full-stack API for telematics devices) provides methods
 to create and manage the flespi telematics hub that jointly provide the
 end-to-end communication between the GPS trackers and your target application.
-This module covers an API for a Device instance of flespi platform. 
+This module covers an API for a Device instance of flespi platform.
 
-Devices is a virtual representations of the physical trackers on the flespi platform. 
-It allows organizing messages by device (ident), remote device configuration, 
+Devices is a virtual representations of the physical trackers on the flespi platform.
+It allows organizing messages by device (ident), remote device configuration,
 dedicated long-term storage, access to telemetry.
 
 More information on `devices <https://flespi.com/flespi-devices>`_ can be
@@ -14,7 +14,7 @@ found on official webpage of a platform.
 
 
 import sys
-#import json
+import json
 import requests
 
 
@@ -59,9 +59,9 @@ class Device:
             'Accept': 'application/json',
             'Authorization': 'FlespiToken {}'.format(self.flespi_token)}
 
-    def _get_handler(self, link):
+    def _get_handler(self, link, params=None):
         try:
-            response = requests.get(link, headers=self.headers)
+            response = requests.get(link, params, headers=self.headers)
             if response.status_code == 200:
                 print("Success!")
             elif response.status_code == 401:
@@ -84,47 +84,45 @@ class Device:
     def _put_handler(self, link):
         pass
 
-    def get_telemetry(self):
-        #curl -X GET  --header 'Accept: application/json' --header 'Authorization: FlespiToken XXXXXXXXX'  'https://flespi.io/gw/devices/{dev-selector}/telemetry'
-        """Python wrapper on flesp API to get the latest telemetry json.
-
-        Returns
-        -------
-        telemetry : dict
-            The latest telemetry from the device.
-
-        Examples
-        --------
-        >>> telemetry = dv.get_telemetry()
-        {'result': [{'id': 123456,
-        'telemetry': {'battery.current': {'ts': 1609610197, 'value': 0},
-        'battery.voltage': {'ts': 1609610197, 'value': 4.043},
-        'can.absolute.load': {'ts': 1609605673, 'value': 17},
-        'can.ambient.air.temperature': {'ts': 1609605673, 'value': 3}]}
-        """
-        link = 'https://flespi.io/gw/devices/{}/telemetry'.format(self.device_number)
-        telemetry = self._get_handler(link=link)
-        return telemetry
-
-    def get_snapshots(self):
-        """Snapshots
-
-        Returns
-        -------
-        snapshots : dict
-            The dict object containing the timestamp when 
-            the snapshot was created.
-        """
-        # curl -X GET  --header 'Accept: application/json' --header 'Authorization: FlespiToken XXXXXXXXX'  'https://flespi.io/gw/devices/{dev-selector}/snapshots''
-        link = 'https://flespi.io/gw/devices/{}/snapshots'.format(self.device_number)
-        snapshots = self._get_handler(link=link)
-        return snapshots
-
     def get_logs(self):
+        """Get logs for specified device. 
+        The request without parameters will return all logs records.
+        By default retreieves all logs. Filtering by parameters
+        is not imlemented
+
+        Returns
+        -------
+        logs : dict
+            Logs for a specific device.
+        """
         # curl -X GET  --header 'Accept: application/json' --header 'Authorization: FlespiToken XXXXXXXXX'  'https://flespi.io/gw/devices/{dev-selector}/logs'
         link = 'https://flespi.io/gw/devices/{}/logs'.format(self.device_number)
         logs = self._get_handler(link=link)
         return logs
+
+    def get_settings(self, all=True):
+        """Get collection of settings matching filter parameters.
+        By default retreieves all settings. Filtering by parameters
+        is not implemented.
+
+        Parameters
+        ----------
+        all : bool
+            Indicates of all settings retreival.
+
+        Returns
+        -------
+        settings : dict
+            Setting of a cpecified device.
+        """
+        #curl -X GET  --header 'Accept: application/json' --header 'Authorization: FlespiToken XXXXXXXXX'  'https://flespi.io/gw/devices/{dev-selector}/settings/{sett-selector}'
+        if all:
+            link = 'https://flespi.io/gw/devices/{}/settings/all'.format(self.device_number)
+            settings = self._get_handler(link=link)
+            return settings
+        else:
+            raise NotImplementedError
+
     def get_snapshot(self, snapshot, output):
         """Fetches snapshot file available for device messages.
 
@@ -155,14 +153,104 @@ class Device:
 
         # return None
         #TODO resolve a problem with saving response properly
-        pass
 
-    def get_devices(self, all_devices=False):
+    def get_messages(self, params=None):
+        #curl -X GET  --header 'Accept: application/json' --header 'Authorization: FlespiToken XXXXXXXXX'  'https://flespi.io/gw/devices/{dev-selector}/messages'
+        """Get specified device messages. The request without
+        parameters will return all device messages.
+        Reply contains sorted by time messages, in case
+        you request messages from multiple devices simulateniously
+        they are not sorted by device.
+
+        Parameters
+        ----------
+        params : dict
+            Dictionary with filter parameters.
+
+        Returns
+        -------
+        messages : dict
+            Messages with telemetry and gps data.
+
+        Examples
+        --------
+        Example of a data retrieveng within a certain time
+        period. The data are sorted according to timestamp.
+        The full description of parameters can be found `here <https://flespi.io/docs/#/gw/devices/get_devices_dev_selector_messages>`_
+
+        >>> params = {'from': 1609581600 , 'to': 1609588800, 'reverse': True}
+        >>> messages = get_messages(params=params)
+        >>> print(messages['result'][100])
+        {'battery.current': 0, 'battery.voltage': 4.044, 'can.absolute.load': 70,
+        'can.ambient.air.temperature': 3 ... }
+        """
+        link = "https://flespi.io/gw/devices/{}/messages".format(self.device_number)
+        if params:
+            params = (('data', '{}'.format(json.dumps(params))),)
+            messages = self._get_handler(link=link, params=params)
+        else:
+            messages = self._get_handler(link=link, params=None)
+        return messages
+
+    def get_telemetry(self):
+        #curl -X GET  --header 'Accept: application/json' --header 'Authorization: FlespiToken XXXXXXXXX'  'https://flespi.io/gw/devices/{dev-selector}/telemetry'
+        """Get specified device telemetry - e.g. latest values of parameters
+        from registered messages. Telemetry is updated
+        automatically during new messages reception for a device.
+
+        Returns
+        -------
+        telemetry : dict
+            The latest telemetry from the device.
+
+        Examples
+        --------
+        >>> telemetry = dv.get_telemetry()
+        >>> print(telemetry)
+        {'result': [{'id': 123456,
+        'telemetry': {'battery.current': {'ts': 1609610197, 'value': 0},
+        'battery.voltage': {'ts': 1609610197, 'value': 4.043},
+        'can.absolute.load': {'ts': 1609605673, 'value': 17},
+        'can.ambient.air.temperature': {'ts': 1609605673, 'value': 3}]}
+        """
+        link = 'https://flespi.io/gw/devices/{}/telemetry'.format(self.device_number)
+        telemetry = self._get_handler(link=link)
+        return telemetry
+
+    def get_devices(self, all=False):
+        """Fetches a collection of devices matching filter parameters.
+        By defaults retreieves all devices.
+
+        Parameters
+        ----------
+        all : bool
+            Defaults to `False`. In this case
+            only initialized device will be returned.
+
+        Returns
+        -------
+        devices : dict
+            Fetched devices.
+        """
         #curl -X GET  --header 'Accept: application/json' --header 'Authorization: FlespiToken XXXXXXXXX'  'https://flespi.io/gw/devices/all'
-        if all_devices:
+        if all:
             link = "https://flespi.io/gw/devices/all"
         else:
             link = "https://flespi.io/gw/devices/{}".format(self.device_number)
 
         devices = self._get_handler(link=link)
         return devices
+
+    def get_snapshots(self):
+        """List of archived messages snapshots available for a device.
+
+        Returns
+        -------
+        snapshots : dict
+            The dict object containing the timestamp when
+            the snapshot was created.
+        """
+        # curl -X GET  --header 'Accept: application/json' --header 'Authorization: FlespiToken XXXXXXXXX'  'https://flespi.io/gw/devices/{dev-selector}/snapshots''
+        link = 'https://flespi.io/gw/devices/{}/snapshots'.format(self.device_number)
+        snapshots = self._get_handler(link=link)
+        return snapshots
--- flespi_gateway/utils.py
+++ flespi_gateway/utils.py
@@ -1,4 +1,4 @@
-"""Helpers to work with python wrapper of flespi api 
+"""Helpers to work with python wrapper of flespi api
 """
 
 
@@ -28,22 +28,29 @@ def convert_unix_ts(timestamp, timezone = "Europe/Berlin"):
 
     return date.strftime('%Y-%m-%d %H:%M:%S')
 
-def convert_human_ts(timestamp):
-    """Utility function to help converting flespi utc unix time output to human readable.
+def convert_human_ts(timestamp, timezone = "Europe/Berlin"):
+    """Utility function to help converting user given timestamp to flespi utc unix time.
 
     Parameters
     ----------
     timestamp : str
-        Unix time generated py flespi platform.
+        Human readable timestamp.
+        The following format is supported `2021-01-02 10:00:00`.
 
     Returns
     -------
     date : int
-        Human readable time with a following format: %Y-%m-%d %H:%M:%S
+        Unix timestamp.
+
+    Examples
+    --------
+    
+    >>> ts = convert_human_ts('2021-01-02 10:00:00')
+    >>> print(ts)
+    1609578000
     """
 
-    # timezone = pytz.timezone(timezone)
-    # date = datetime.fromtimestamp(timestamp, timezone)
-
-    # return date.strftime('%Y-%m-%d %H:%M:%S')
-    pass
+    timezone = pytz.timezone(timezone)
+    date_time_obj = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+    timezone_date_time_obj = timezone.localize(date_time_obj)
+    return int(timezone_date_time_obj.timestamp())
