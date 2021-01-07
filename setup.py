24cebbdc58343e5d1ce4581b4247b8c1de2a0642
vlavrik <lavrikvladimir@gmail.com>
2021-01-07 12:07:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,4 +15,4 @@ setuptools.setup(
     license='BSD 3-Clause "New" or "Revised" License',
     packages=setuptools.find_packages(),
     zip_safe=False,
-    python_requires='>=3.7')
+    python_requires='>=3.6')
