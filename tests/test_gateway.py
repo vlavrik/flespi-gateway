5fd9cb806d8fc16d6c8b0c4778b9a1353db8c9ea
Vladimir S. Lavrik <vlavrik@Vladimirs-MacBook-Air.local>
2021-01-07 13:46:36 +0100

    placeholder test case removed

--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -7,25 +7,12 @@ class TestDevice(unittest.TestCase):
     def setUp(self):
         FLESPI_TOKEN = os.environ.get('FLESPI_TOKEN', None)
         FLESPI_DEVICE_NUMBER = int(os.environ.get('DEVICE', None))
-        print(FLESPI_TOKEN)
         self.dv = Device(device_number=FLESPI_DEVICE_NUMBER, flespi_token=FLESPI_TOKEN)
-    
-    def test_list_int(self):
-        """
-        Test that it can sum a list of integers
-        """
-        data = [1, 2, 3]
-        result = sum(data)
-        self.assertEqual(result, 6)
 
     def test_telemetry(self):
         telemetry = self.dv.get_telemetry()
 
         self.assertIsInstance(telemetry, dict)
-
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
