923adead6d7bdc3ee0b08fbcb71fb76758e7af46
vlavrik <lavrikvladimir@gmail.com>
2021-01-07 09:45:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,4 +14,4 @@ setuptools.setup(name='flespi-gateway',
       license='BSD 3-Clause "New" or "Revised" License',
       packages=setuptools.find_packages(),
       zip_safe=False,
-      python_requires='>=3.8')
+      python_requires='>=3.7')
