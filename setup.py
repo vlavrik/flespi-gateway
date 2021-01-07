7104f0c517e332d173527a0a73b50adf8479f427
Vladimir S. Lavrik <vlavrik@Vladimirs-MacBook-Air.local>
2021-01-07 11:21:06 +0100

    setup.py reformatting

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(name='flespi-gateway',
-      version='0.0.7',
-      description='The python wrapper of a gateway rest API of a flespi platform.',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/vlavrik/flespi-gateway',
-      author='Vladimir Lavrik',
-      author_email='lavrikvladimir@gmail.com',
-      license='BSD 3-Clause "New" or "Revised" License',
-      packages=setuptools.find_packages(),
-      zip_safe=False,
-      python_requires='>=3.7')
+setuptools.setup(
+    name='flespi-gateway',
+    version='0.0.7',
+    description='The python wrapper of a gateway rest API of a flespi platform.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/vlavrik/flespi-gateway',
+    author='Vladimir Lavrik',
+    author_email='lavrikvladimir@gmail.com',
+    license='BSD 3-Clause "New" or "Revised" License',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    python_requires='>=3.7')
